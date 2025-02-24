742b383b6d806fd4feada755197cfffbb4bb4606
Klaus <niklauswy@gmail.com>
2025-02-23 21:18:31 -0800

    nvim

---
+++ home/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false, -- Forzar carga inmediata
+    event = "InsertEnter", -- Cargarlo al entrar en modo Insert
+    config = function()
+      vim.g.copilot_no_tab_map = false  -- Evitar conflictos con Tab
+    end
+  }
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
